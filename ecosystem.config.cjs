e07664448130de157dece0f3ab1baa41097ca43f
hc <513416443@qq.com>
2023-03-20 16:40:41 +0800

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'wxbot',
             args: '5555',
-            script: './index.js',
+            script: './bot.js',
             log_date_format: 'YYYY-MM-DD HH:mm Z'
         }
     ]
